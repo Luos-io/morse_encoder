4f0e402cb5e76727c0d1272bc30d81b1b5230588
biliouriv <biliouriv@gmail.com>
2022-01-12 16:40:55 +0100

    Led Driver

---
+++ Morse_Encoder/lib/Led/led.c
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * @file Led
+ * @brief driver example a simple Led
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#include "led.h"
+#include "led_drv.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Functions
+ ******************************************************************************/
+static void Led_MsgHandler(service_t *service, msg_t *msg);
+/******************************************************************************
+ * @brief service init must be call in project init
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Init(void)
+{
+    // example revision
+    revision_t revision = {.major = 1, .minor = 0, .build = 0};
+    // Initialization of Led Driver
+    LedDrv_Init();
+    // Create Led service
+    Luos_CreateService(Led_MsgHandler, STATE_TYPE, "led_service", revision);
+}
+/******************************************************************************
+ * @brief service loop must be call in project loop
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Loop(void)
+{
+}
+/******************************************************************************
+ * @brief Msg manager callback when a msg receive for the led service
+ *        Led can receive messages commands of type COLOR
+ * @param Service destination
+ * @param Msg receive
+ * @return None
+ ******************************************************************************/
+static void Led_MsgHandler(service_t *service, msg_t *msg)
+{
+    if (msg->header.cmd == IO_STATE)
+    {
+        // get state value from message
+        uint8_t state = msg->data[0];
+        // Send the state value to the led
+        LedDrv_Write(state);
+    }
+}
