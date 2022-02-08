2db1545f2c1c3131ed0e4fb432a79bc0a9c5b0be
Salem_Luos <thomas.salembier@luos.io>
2022-02-08 19:02:45 +0100

    add basic project

---
+++ Basic_Project/node_config.h
@@ -0,0 +1,11 @@
+/******************************************************************************
+ * @file node_config.h
+ * @brief This file allow you to use standard preprocessor definitions to
+ *        configure your project, Luos and Luos HAL libraries
+ * @author Yourself
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef _NODE_CONFIG_H_
+#define _NODE_CONFIG_H_
+
+#endif /* _NODE_CONFIG_H_ */
